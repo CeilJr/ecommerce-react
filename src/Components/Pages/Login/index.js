5d315840a73d0b72b71dfac97f0dd2edeabc924f
Ceil Campos Junior <ceilcamposjunior@gmail.com>
2022-11-08 00:15:32 -0300

    Login-Atualizado-8-11-22-Ceil

--- src/Components/Pages/Login/index.js
+++ src/Components/Pages/Login/index.js
@@ -1,15 +1,11 @@
 import React from "react";
-import styled from "styled-components";
 import {Container, Button, Input} from "./style.js"
-import { useNavigate } from "react-router-dom";
 
 export const Login = () => {
 
     return(
         <>
             <Container>
-            {/* <img src="https://cdn.xxl.thumbs.canstockphoto.com.br/encontrar-pessoas-logo-trabalho-equipe-c%C3%ADrculo-grupo-seis-sala-pessoas-desenho_csp68862395.jpg" 
-            alt="imagem do grupo seis" height="50px"></img> */}
                 <div>
                     <div>E-mail:</div>
                     <Input type="text" placeholder="Digite seu e-mail" required 
